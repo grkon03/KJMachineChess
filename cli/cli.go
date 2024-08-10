a92820d34cbb8ec8af1eebe510ec955bf133e99c
grkon <yo.for.user@gmail.com>
2024-08-10 14:10:47 +0900

    impl a few of cli

--- cli/cli.go
+++ cli/cli.go
@@ -1,5 +1,41 @@
 package cli
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 type CLI struct {
-	Proecesses []Process
+	Proecesses      []Process
+	CommandsHistory []string
+
+	ProcessingCommand string
+}
+
+const launchLogo string = `
+---------------------------------
+KJMachine for chess
+	from grkon
+---------------------------------
+`
+
+const splitLine string = "---------------------"
+
+func NewCLI() CLI {
+	fmt.Print(launchLogo)
+	return CLI{}
+}
+
+func (cli *CLI) ReceiveCommand() {
+	fmt.Println(splitLine)
+	fmt.Println("(=°ω°=) < Meneow")
+	fmt.Println(splitLine)
+
+	fmt.Print("> ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	cli.ProcessingCommand = scanner.Text()
+	cli.CommandsHistory = append([]string{cli.ProcessingCommand}, cli.CommandsHistory...)
 }
